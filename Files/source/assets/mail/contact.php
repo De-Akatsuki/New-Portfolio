3265cff46eb75922207176fc17494c70a3549880
Herr-Akatsuki <85358793+De-Akatsuki@users.noreply.github.com>
2023-11-18 00:09:41 +0000

    Modified Content

--- Files/source/assets/mail/contact.php
+++ Files/source/assets/mail/contact.php
@@ -42,7 +42,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "support@validthemes.live";
+$address = "kwamecorchil58@gmail.com";
 
 
 // Configuration option.
